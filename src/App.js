3a7683c1477f14101edb11644e31cd7bb7b29384
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-11 08:10:30 +0100

    Fetching API

--- src/App.js
+++ src/App.js
@@ -5,17 +5,40 @@ import "./index.css";
 import TimeAndLocation from "./component/TimeAndLocation";
 import TemperatureAndDetails from "./component/TemperatureAndDetails";
 import Forecast from "./component/Forecast";
+import getFormattedWeatherData from "./Service/WeatherService";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const data = await getFormattedWeatherData({ ...query, units }).then(
+        (data) => {
+          setWeather(data);
+        }
+      );
+      console.log(data);
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  console.log(weather);
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shodow-xl shadow-grey-400">
       <TopButton />
       <Inputs />
-
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-      <Forecast title="hourly forecast" />
-      <Forecast title="daily forecast" />
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+          <Forecast title="hourly forecast" />
+          <Forecast title="daily forecast" />
+        </>
+      )}
     </div>
   );
 }
